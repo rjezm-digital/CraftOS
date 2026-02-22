b13dcb1807411367b94024a53a3bb416ae10ce8b
rjezm-digital <rjezm.tadlas@gmail.com>
2026-02-22 13:13:37 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDSA9E4nDca50a1WcmNLFLXudI-PCOfFbg",
+  authDomain: "rjezm-print-os.firebaseapp.com",
+  projectId: "rjezm-print-os",
+  storageBucket: "rjezm-print-os.firebasestorage.app",
+  messagingSenderId: "871926743321",
+  appId: "1:871926743321:web:1d46bf4351b497fd738588"
 };
 
 const app = initializeApp(firebaseConfig);
